c7279affff5cf8199a1d1dde30365318b8c4a28a
Adrián Fernández Rosa <adrianfr2011@gmail.com>
2022-05-21 00:35:15 -0300

    fix properties

--- src/main/java/com/alkemy/ong/repository/UserRepository.java
+++ src/main/java/com/alkemy/ong/repository/UserRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
-
+// 
     User findByEmail(String email);
 
     Optional<User> findById(Long id);
