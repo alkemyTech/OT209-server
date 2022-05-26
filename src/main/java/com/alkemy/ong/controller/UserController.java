f767b147dc8d25b372f744abb440313544b8fa44
hitechStart <mdlprofesional@gmail.com>
2022-05-26 12:49:33 -0300

    incidencias 29-31-33 finalizadas

--- src/main/java/com/alkemy/ong/controller/UserController.java
+++ src/main/java/com/alkemy/ong/controller/UserController.java
@@ -5,6 +5,8 @@ package com.alkemy.ong.controller;
 
 import javax.validation.Valid;
 
+import com.alkemy.ong.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,8 +19,10 @@ import com.alkemy.ong.models.request.AuthenticateRequest;
 import com.alkemy.ong.models.request.RegisterRequest;
 import com.alkemy.ong.models.response.AuthenticateResponse;
 import com.alkemy.ong.models.response.RegisterResponse;
-import com.alkemy.ong.service.UserService;
+import com.alkemy.ong.service.impl.UserServiceImpl;
 
+import java.io.IOException;
+@Slf4j
 @RestController
 @RequestMapping("/auth")
 public class UserController {
@@ -27,12 +31,14 @@ public class UserController {
 	private UserService userService;
 
 	@PostMapping("/register")
-	public ResponseEntity<RegisterResponse> signUp(@Valid @RequestBody RegisterRequest registerRequest) {
+	public ResponseEntity<RegisterResponse> signUp(@Valid @RequestBody RegisterRequest registerRequest) throws IOException {
+		log.info("enviando Imagen de bienvenida");
+		userService.send();
 		return ResponseEntity.status(HttpStatus.CREATED).body(userService.register(registerRequest));
 	}
 
 	@PostMapping("/login")
-	public ResponseEntity<AuthenticateResponse>  signIn(@RequestBody AuthenticateRequest authRequest) {
+	public ResponseEntity<AuthenticateResponse>  signIn(@RequestBody AuthenticateRequest authRequest) throws Exception {
 		return ResponseEntity.ok(userService.login(authRequest.getEmail(),authRequest.getPassword()));
 		
 	}
