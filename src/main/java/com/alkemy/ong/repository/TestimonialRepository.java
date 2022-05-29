ee0ae6d406189a784e28c1b84c967d5b1db2bc3b
Ignacio <joseignacio439@gmail.com>
2022-05-29 17:36:39 -0300

    try resolve lines blank in repository

--- src/main/java/com/alkemy/ong/repository/TestimonialRepository.java
+++ src/main/java/com/alkemy/ong/repository/TestimonialRepository.java
@@ -2,7 +2,6 @@ package com.alkemy.ong.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.alkemy.ong.models.entity.TestimonialEntity;
 @Repository
 public interface TestimonialRepository extends JpaRepository<TestimonialEntity, Long>{
