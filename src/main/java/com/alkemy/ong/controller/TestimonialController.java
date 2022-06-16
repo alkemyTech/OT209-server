1ee72f7e00eb20a7b28e6a056003a766739eabae
Adrián Fernández Rosa <adrianfr2011@gmail.com>
2022-06-16 17:34:55 -0300

    testimonial docs

--- src/main/java/com/alkemy/ong/controller/TestimonialController.java
+++ src/main/java/com/alkemy/ong/controller/TestimonialController.java
@@ -1,11 +1,17 @@
 
 package com.alkemy.ong.controller;
 
+
 import com.alkemy.ong.models.request.TestimonialRequest;
 import com.alkemy.ong.models.response.PageTestimonialResponse;
 import com.alkemy.ong.models.response.TestimonialResponse;
 import com.alkemy.ong.service.TestimonialService;
 import javax.validation.Valid;
+import io.swagger.annotations.*;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.tags.Tags;
+import com.alkemy.ong.auth.config.SwaggerConfig;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +22,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 @CrossOrigin("*")
 @RequestMapping("/testimonials")
 @RequiredArgsConstructor
+@Api(tags = {SwaggerConfig.TESTIMONIAL_CONTROLLER})
 public class TestimonialController {
 
     private static final String TESTIMONIALS_PATH = "/testimonials";
@@ -23,14 +30,25 @@ public class TestimonialController {
     private final TestimonialService testimonialService;
     
     @PostMapping("")
+    @ApiOperation(value = "Create Testimonials", notes = "Allows user to insert news testimonial ")
     @ResponseStatus(HttpStatus.CREATED)
-    public TestimonialResponse create(@Valid @RequestBody TestimonialRequest request){
+    public TestimonialResponse create(@Valid @RequestBody @ApiParam(
+            name= "New Testimonial",
+            value = "New testimonial to save",
+            required = true)
+         TestimonialRequest request){
         return testimonialService.save(request);
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public TestimonialResponse update(@PathVariable Long id, @RequestBody TestimonialRequest request){
+    @ApiOperation(value = "Update testimonials by ID",notes = "Allows user to update an existing testimonials by ID")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Testimonial updated!"),
+            @ApiResponse(code = 404, message = "No Testimonial with requested ID is found"),
+    })
+    public TestimonialResponse update(@PathVariable Long id,
+                                    @RequestBody TestimonialRequest request){
        return testimonialService.update(id, request);
     }
 
