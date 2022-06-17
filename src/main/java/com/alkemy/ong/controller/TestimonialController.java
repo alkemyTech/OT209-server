a8123ae7cc96e69ce8833d9963f505716ec1ee1d
Adrián Fernández Rosa <adrianfr2011@gmail.com>
2022-06-16 23:00:20 -0300

    testimonial docs

--- src/main/java/com/alkemy/ong/controller/TestimonialController.java
+++ src/main/java/com/alkemy/ong/controller/TestimonialController.java
@@ -8,8 +8,6 @@ import com.alkemy.ong.models.response.TestimonialResponse;
 import com.alkemy.ong.service.TestimonialService;
 import javax.validation.Valid;
 import io.swagger.annotations.*;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.tags.Tags;
 import com.alkemy.ong.auth.config.SwaggerConfig;
 
 import lombok.RequiredArgsConstructor;
@@ -30,7 +28,7 @@ public class TestimonialController {
     private final TestimonialService testimonialService;
     
     @PostMapping("")
-    @ApiOperation(value = "Create Testimonials", notes = "Allows user to insert news testimonial ")
+    @ApiOperation(value = "Create Testimonials", notes = "Allows user to insert new testimonial ")
     @ResponseStatus(HttpStatus.CREATED)
     public TestimonialResponse create(@Valid @RequestBody @ApiParam(
             name= "New Testimonial",
@@ -47,20 +45,52 @@ public class TestimonialController {
             @ApiResponse(code = 200, message = "Testimonial updated!"),
             @ApiResponse(code = 404, message = "No Testimonial with requested ID is found"),
     })
-    public TestimonialResponse update(@PathVariable Long id,
-                                    @RequestBody TestimonialRequest request){
+    public TestimonialResponse update(@PathVariable
+                                          @ApiParam(
+                                          name =  "ID",
+                                          type = "Long",
+                                          value = "ID of the testimonial requested",
+                                          example = "1",
+                                          required = true) Long id,
+                                    @RequestBody
+                                    @ApiParam(
+                                            name =  "New Testimonials",
+                                            value = "testimonials to save",
+                                            required = true)
+                                            TestimonialRequest request){
        return testimonialService.update(id, request);
     }
 
     @DeleteMapping("/{id}")
+    @ApiOperation(value = "Soft delete testimonial by id", notes ="Allows user to delete testimonial by ID")
+    @ApiResponses({
+            @ApiResponse(code = 204, message = "Testimonial soft deleted!"),
+            @ApiResponse(code = 404, message = "No testimonial with requested ID is found"),
+    })
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteById(@PathVariable Long id) {
+    public void deleteById(@PathVariable
+                             @ApiParam(
+                             name =  "ID",
+                             type = "Long",
+                             value = "ID of the testimonial requested",
+                            example = "1",
+                            required = true) Long id) {
         testimonialService.delete(id);
     }
 
     @GetMapping
+    @ApiOperation(value = "Get All Testimonials", notes = "Returns all Testimonials")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Returns all Testimonials")
+    })
     public ResponseEntity<PageTestimonialResponse> getAll(
-            @RequestParam(value = "page") int offset,
+            @RequestParam(value = "page")
+            @ApiParam(
+                    name =  "Page #",
+                    type = "Integer",
+                    value = "Page requested from the list of testimonial (starting in 1)",
+                    example = "1",
+                    required = true) int offset,
             UriComponentsBuilder uriComponentsBuilder
     ){
 
@@ -69,7 +99,18 @@ public class TestimonialController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<TestimonialResponse> getDetail(@PathVariable Long id){
+    @ApiOperation(value = "Get Testimonial By ID", notes = "Returns one testimonial by ID")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Return the requested testimonials"),
+            @ApiResponse(code = 404, message = "No testimonials with requested ID is found"),
+    })
+    public ResponseEntity<TestimonialResponse> getDetail(@PathVariable
+                                                             @ApiParam(
+                                                             name =  "ID",
+                                                             type = "Long",
+                                                             value = "ID of the testimonial requested",
+                                                             example = "1",
+                                                             required = true) Long id){
         TestimonialResponse response = testimonialService.getTestimonial(id);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
