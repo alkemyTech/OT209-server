4065908914a27837718d003acf06a7f95bdbe7e1
Matias Solari <solarimatias_@hotmail.com>
2022-06-22 22:35:42 -0300

    Added documentation for slides

--- src/main/java/com/alkemy/ong/models/response/SlideBasicResponse.java
+++ src/main/java/com/alkemy/ong/models/response/SlideBasicResponse.java
@@ -2,6 +2,8 @@ package com.alkemy.ong.models.response;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,9 +11,17 @@ import lombok.Setter;
 @Setter
 public class SlideBasicResponse {
 
+	@ApiModelProperty(name = "imageUrl",
+            value = "amazon url for image",
+            dataType = "String",
+            example = "amazon url for image")
     @NotEmpty(message = "El campo imageUrl debe completarlo")
     @NotBlank
     private String imageUrl;
 
+	@ApiModelProperty(name = "slideOrder",
+            value = "order for slide",
+            dataType = "Integer",
+            example = "1")
     private int slideOrder;
 }
