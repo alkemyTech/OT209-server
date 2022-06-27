f05573bd353b2318f74b182816eca976373d3399
Adrián Fernández Rosa <adrianfr2011@gmail.com>
2022-06-27 19:12:50 -0300

    load data sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,30 @@
+INSERT INTO `alkemy_ong`.`testimonials` (`testimonial_id`, `content`, `date`, `image`, `name`, `soft_delete`) VALUES ('343', 'Gracias ONG Somos mas, este es el testimonial 1', now(), 'image1.jpg', 'testimonial 1', b'0');
+INSERT INTO `alkemy_ong`.`testimonials` (`testimonial_id`, `content`, `date`, `image`, `name`, `soft_delete`) VALUES ('344', 'Gracias ONG Somos mas, este es el testimonial 2', now(), 'image1.jpg', 'testimonial 2', b'0');
+INSERT INTO `alkemy_ong`.`testimonials` (`testimonial_id`, `content`, `date`, `image`, `name`, `soft_delete`) VALUES ('345', 'Gracias ONG Somos mas, este es el testimonial 3', now(), 'image1.jpg', 'testimonial 3', b'0');
+INSERT INTO `alkemy_ong`.`testimonials` (`testimonial_id`, `content`, `date`, `image`, `name`, `soft_delete`) VALUES ('346', 'Gracias ONG Somos mas, este es el testimonial 4', now(), 'image1.jpg', 'testimonial 4', b'0');
+INSERT INTO `alkemy_ong`.`testimonials` (`testimonial_id`, `content`, `date`, `image`, `name`, `soft_delete`) VALUES ('347', 'Gracias ONG Somos mas, este es el testimonial 5', now(), 'image1.jpg', 'testimonial 5', b'0');
+INSERT INTO `alkemy_ong`.`testimonials` (`testimonial_id`, `content`, `date`, `image`, `name`, `soft_delete`) VALUES ('348', 'Gracias ONG Somos mas, este es el testimonial 6', now(), 'image1.jpg', 'testimonial 6', b'0');
+INSERT INTO `alkemy_ong`.`testimonials` (`testimonial_id`, `content`, `date`, `image`, `name`, `soft_delete`) VALUES ('349', 'Gracias ONG Somos mas, este es el testimonial 7', now(), 'image1.jpg', 'testimonial 7', b'0');
+INSERT INTO `alkemy_ong`.`testimonials` (`testimonial_id`, `content`, `date`, `image`, `name`, `soft_delete`) VALUES ('350', 'Gracias ONG Somos mas, este es el testimonial 8', now(), 'image1.jpg', 'testimonial 8', b'0');
+INSERT INTO `alkemy_ong`.`testimonials` (`testimonial_id`, `content`, `date`, `image`, `name`, `soft_delete`) VALUES ('351', 'Gracias ONG Somos mas, este es el testimonial 9', now(), 'image1.jpg', 'testimonial 9', b'0');
+
+INSERT INTO `alkemy_ong`.`members` (`id`, `description`, `image`, `name`, `soft_delete`, `timestamp`) VALUES ('341', 'Colaborador 2', 'foto2.jpg', 'nombre2', b'0', now());
+INSERT INTO `alkemy_ong`.`members` (`id`, `description`, `image`, `name`, `soft_delete`, `timestamp`) VALUES ('342', 'Colaborador 3', 'foto3.jpg', 'nombre3', b'0', now());
+INSERT INTO `alkemy_ong`.`members` (`id`, `description`, `image`, `name`, `soft_delete`, `timestamp`) VALUES ('343', 'Colaborador 4', 'foto4.jpg', 'nombre4', b'0', now());
+INSERT INTO `alkemy_ong`.`members` (`id`, `description`, `image`, `name`, `soft_delete`, `timestamp`) VALUES ('344', 'Colaborador 5', 'foto5.jpg', 'nombre5', b'0', now());
+INSERT INTO `alkemy_ong`.`members` (`id`, `description`, `image`, `name`, `soft_delete`, `timestamp`) VALUES ('345', 'Colaborador 6', 'foto6.jpg', 'nombre6', b'0', now());
+INSERT INTO `alkemy_ong`.`members` (`id`, `description`, `image`, `name`, `soft_delete`, `timestamp`) VALUES ('346', 'Colaborador 7', 'foto7.jpg', 'nombre7', b'0', now());
+INSERT INTO `alkemy_ong`.`members` (`id`, `description`, `image`, `name`, `soft_delete`, `timestamp`) VALUES ('347', 'Colaborador 8', 'foto8.jpg', 'nombre8', b'0', now());
+
+INSERT INTO `alkemy_ong`.`categories` (`category_id`, `description`, `image`, `name`, `soft_delete`, `timestamp`) VALUES ('1', 'actualidad', 'image120.jpg', 'actualidad', b'0', now());
+
+INSERT INTO `alkemy_ong`.`news` (`news_id`, `category_id`, `content`, `image`, `name`, `soft_delete`, `timestamp`) VALUES ('1', '1', 'noticia 1 esta es una noticia de la ong somos más.', 'image1.jpg', 'ONG SOMOS MAS SIGUE CRECIENDO', b'0', now());
+INSERT INTO `alkemy_ong`.`news` (`news_id`, `category_id`, `content`, `image`, `name`, `soft_delete`, `timestamp`) VALUES ('2', '1', 'noticia 2 esta es una noticia de la ong somos más.', 'image2.jpg', 'ONG SOMOS MAS SIGUE CRECIENDO', b'0', now());
+INSERT INTO `alkemy_ong`.`news` (`news_id`, `category_id`, `content`, `image`, `name`, `soft_delete`, `timestamp`) VALUES ('3', '1', 'noticia 3 esta es una noticia de la ong somos más.', 'image3.jpg', 'ONG SOMOS MAS SIGUE CRECIENDO', b'0', now());
+INSERT INTO `alkemy_ong`.`news` (`news_id`, `category_id`, `content`, `image`, `name`, `soft_delete`, `timestamp`) VALUES ('4', '1', 'noticia 4 esta es una noticia de la ong somos más.', 'image4.jpg', 'ONG SOMOS MAS SIGUE CRECIENDO', b'0', now());
+INSERT INTO `alkemy_ong`.`news` (`news_id`, `category_id`, `content`, `image`, `name`, `soft_delete`, `timestamp`) VALUES ('5', '1', 'noticia 5 esta es una noticia de la ong somos más.', 'image5.jpg', 'ONG SOMOS MAS SIGUE CRECIENDO', b'0', now());
+INSERT INTO `alkemy_ong`.`news` (`news_id`, `category_id`, `content`, `image`, `name`, `soft_delete`, `timestamp`) VALUES ('6', '1', 'noticia 6 esta es una noticia de la ong somos más.', 'image6.jpg', 'ONG SOMOS MAS SIGUE CRECIENDO', b'0', now());
+INSERT INTO `alkemy_ong`.`news` (`news_id`, `category_id`, `content`, `image`, `name`, `soft_delete`, `timestamp`) VALUES ('7', '1', 'noticia 7 esta es una noticia de la ong somos más.', 'image7.jpg', 'ONG SOMOS MAS SIGUE CRECIENDO', b'0', now());
+INSERT INTO `alkemy_ong`.`news` (`news_id`, `category_id`, `content`, `image`, `name`, `soft_delete`, `timestamp`) VALUES ('8', '1', 'noticia 8 esta es una noticia de la ong somos más.', 'image8.jpg', 'ONG SOMOS MAS SIGUE CRECIENDO', b'0', now());
+
+INSERT INTO `alkemy_ong`.`news` (`news_id`, `category_id`, `content`, `image`, `name`, `soft_delete`, `timestamp`) VALUES ('9', '1', 'noticia 9 esta es una noticia de la ong somos más.', 'image9.jpg', 'ONG SOMOS MAS SIGUE CRECIENDO', b'0', now());
